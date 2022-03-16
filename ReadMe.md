d6f436a1ea898121ac169ee822d9f34e849fe9df
MHZ <mohamed.hichem.zayani@gmail.com>
2022-03-16 18:54:44 +0100

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Getting Started
+This little project highlights the main capabilities of spring security.
+
+The master branch contains the initial code and each other branch has specific topic.
+
+Enjoy ;)
---
+++ src/main/java/com/example/springsecurity/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
