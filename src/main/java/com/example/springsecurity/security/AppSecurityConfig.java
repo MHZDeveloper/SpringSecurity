7f1df9ba18bce65c34af214ae86acbc653797bc2
MHZ <mohamed.hichem.zayani@gmail.com>
2022-03-16 21:45:50 +0100

    add in-memory authentication

--- src/main/java/com/example/springsecurity/security/AppSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/AppSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity.security;
 
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,5 +17,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user") // noop means there is no password encryption
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}admin") // noop means there is no password encryption
+                .roles("ADMIN");
     }
 }
