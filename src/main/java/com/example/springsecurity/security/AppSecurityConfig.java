1a6d88df024e45fdc8010d0b6b3e1e2ffa2d80c6
MHZ <mohamed.hichem.zayani@gmail.com>
2022-03-16 19:14:31 +0100

    configuring security

---
+++ src/main/java/com/example/springsecurity/security/AppSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.springsecurity.security;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+}
